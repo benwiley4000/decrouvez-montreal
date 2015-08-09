2f830f7b63d9b76b9bc5cdba8d71c5c27aaa35ea
Ben Wiley <therealbenwiley@gmail.com>
2015-08-09 17:01:44 -0400

    Callbacks refactored slightly, console.logs removed.

--- js/app.js
+++ js/app.js
@@ -4,23 +4,25 @@ var service = new google.maps.places.PlacesService(map);
 
 var place = "Montreal, Quebec, Canada";
 
-function detailsCallback(place, status) {
-	if(status === google.maps.places.PlacesServiceStatus.OK) {
-		console.log(place.geometry);
-		var mapOptions = {
-			center: place.geometry.location,
-			zoom: 8
-		};
-		map.setOptions(mapOptions);
-	}
-};
-
 function searchCallback(results, status) {
 	if(status === google.maps.places.PlacesServiceStatus.OK) {
-		var id = results[0].place_id;
-		console.log(id);
-		service.getDetails({placeId: id}, detailsCallback);
+		service.getDetails({
+			placeId: results[0].place_id
+		}, detailsCallback);
 	}
+
+	function detailsCallback(place, status) {
+		if(status === google.maps.places.PlacesServiceStatus.OK) {
+			map.setOptions({
+				center: place.geometry.location,
+				zoom: 10
+			});
+		}
+	};
 };
 
 service.textSearch({query: place}, searchCallback);
+
+//KEEP IN MIND:
+//Map.panTo(LatLng|LatLngLiteral)
+//Map.panToBounds(LatLngBounds)
