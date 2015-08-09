09e9250dd291dfa85e9ab156f7802ae673bddd00
Ben Wiley <therealbenwiley@gmail.com>
2015-08-09 16:50:50 -0400

    App now searches for variable place name and renders map centered on its location.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
-var mapOptions = {
-	center: {
-		lat: -34.397,
-		lng: 150.644
-	},
-	zoom: 8
+var map = new google.maps.Map(document.getElementById('map-canvas'));
+
+var service = new google.maps.places.PlacesService(map);
+
+var place = "Montreal, Quebec, Canada";
+
+function detailsCallback(place, status) {
+	if(status === google.maps.places.PlacesServiceStatus.OK) {
+		console.log(place.geometry);
+		var mapOptions = {
+			center: place.geometry.location,
+			zoom: 8
+		};
+		map.setOptions(mapOptions);
+	}
 };
-var map = new google.maps.Map(document.getElementById('map-canvas'),
-	mapOptions);
+
+function searchCallback(results, status) {
+	if(status === google.maps.places.PlacesServiceStatus.OK) {
+		var id = results[0].place_id;
+		console.log(id);
+		service.getDetails({placeId: id}, detailsCallback);
+	}
+};
+
+service.textSearch({query: place}, searchCallback);
