3a1ba60c017312d0b6c088c9003b24ab4ca44d93
Ben Wiley <therealbenwiley@gmail.com>
2015-08-13 00:25:21 -0400

    Code refactored for Knockout; custom data binding setup for map.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,69 @@
-var map = new google.maps.Map(document.getElementById('map-canvas'));
+var PLACE_NAME = "Montreal, Quebec, Canada";
+var MAP = new google.maps.Map(document.getElementById('map-canvas'));
+var SERVICE = new google.maps.places.PlacesService(MAP);
 
-var service = new google.maps.places.PlacesService(map);
+// the ViewModel
+var ViewModel = function() {
+	var self = this;
 
-var place = "Montreal, Quebec, Canada";
+	self.mapData = {
+		placeName: PLACE_NAME,
+		list: ko.observableArray()
+	};
 
-function searchCallback(results, status) {
-	if(status === google.maps.places.PlacesServiceStatus.OK) {
-		service.getDetails({
-			placeId: results[0].place_id
-		}, detailsCallback);
-	}
+	// when called, adds a new marker to mapData.list
+	self.addMarker = function(place_id, loc, viewport) {
+		self.mapData.list.push({
+			"place_id": place_id,
+			"loc": loc,
+			"viewport": viewport
+		});
+	};
 
-	function detailsCallback(place, status) {
-		if(status === google.maps.places.PlacesServiceStatus.OK) {
-			map.setOptions({
-				center: place.geometry.location,
-				zoom: 10
-			});
-		}
+	// when called, removes all markers with the given place_id property
+	self.removeMarker = function(place_id) {
+		self.mapData.list = ko.observableArray(this.mapData.list.remove(function(place) {
+			return place.place_id === place_id;
+		}));
 	};
 };
 
-service.textSearch({query: place}, searchCallback);
+// specifies custom binding for map
+ko.bindingHandlers.map = {
+	init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+		// gets the mapData object
+		var mapData = valueAccessor();
 
-//KEEP IN MIND:
-//Map.panTo(LatLng|LatLngLiteral)
-//Map.panToBounds(LatLngBounds)
+		// if map center data exists, go ahead and initialize
+		if(mapData.centerData) {
+			MAP.setOptions({
+				center: mapData.centerData.location,
+				zoom: 10
+			});
+		}
+
+		// otherwise, data is searched for
+		else {
+			// callback function (invoked below) sets up initial map properties
+			var mapSetUpCallback = function(results, status) {
+				if(status === google.maps.places.PlacesServiceStatus.OK) {
+					mapData.centerData = results[0].geometry;
+					MAP.setOptions({
+						center: mapData.centerData.location,
+						zoom: 10
+					});
+				}
+			};
+
+			// searches with name of map center and initializes rest of map data
+			SERVICE.textSearch({query: mapData.placeName}, mapSetUpCallback);
+		}
+	},
+	update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+		var mapData = valueAccessor();
+		console.log(mapData.list());
+	}
+};
+
+var vm = new ViewModel();
+ko.applyBindings(vm);
